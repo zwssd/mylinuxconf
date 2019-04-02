c830576f8a92ed0b38c66b9cc9f3d91657d93f9c
zwssd <zwssd1980@163.com>
2019-04-02 19:45:55 +0800

    oldacerinstallubuntu

---
+++ oldacerinstallubuntuafter.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+passwd root
+echo "blacklist acer_wmi"> /etc/modprobe.d/acer.conf
+apt-get install chromium-browser fcitx fcitx-config-gtk2 fcitx-table-wbpy fcitx-ui-light python-pip php composer yarn atom4 vim-gtk shadowsocks filezilla gimp aria2 
