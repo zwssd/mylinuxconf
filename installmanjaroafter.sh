8e2766c1643202b9ed8090b7655a836fdf5baa78
zhangwei <zwssd1980@163.com>
2019-07-31 15:38:35 +0800

    update manjaro

--- installmanjaroafter.sh
+++ installmanjaroafter.sh
@@ -2,3 +2,7 @@
 pacman-mirrors -i -c China -m rank
 pacman -Syyu
 pacman -Sy chromium fcitx fcitx-configtool fcitx-gtk3 fcitx-qt5 fcitx-table-extra fcitx-table-other fcitx-ui-light python-pip php composer yarn atom pycharm-community-edition gvim bluefish shadowsocks-qt5 filezilla gimp aria2 
+
+echo "export GTK_IM_MODULE=fcitx" >> ~/.xprofile
+echo "export QT_IM_MODULE=fcitx" >> ~/.xprofile
+echo 'export XMODIFIERS="@im=fcitx"' >> ~/.xprofile
