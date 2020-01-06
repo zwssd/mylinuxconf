0794068ffe2083462e73e0e7873ea598b02b7f43
David <zwssd1980@163.com>
2020-01-06 18:55:16 +0800

    update install software
    update install software

--- installmanjaroafter.sh
+++ installmanjaroafter.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 pacman-mirrors -i -c China -m rank
 pacman -Syyu
-pacman -Sy chromium fcitx fcitx-configtool fcitx-gtk3 fcitx-qt5 fcitx-table-extra fcitx-table-other fcitx-ui-light python-pip php composer yarn atom pycharm-community-edition gvim bluefish shadowsocks-qt5 filezilla gimp aria2 
+pacman -Sy chromium fcitx fcitx-configtool fcitx-gtk3 fcitx-qt5 fcitx-table-extra fcitx-table-other fcitx-ui-light php php-apache apache mysql phpmyadmin gvim filezilla gimp aria2 code 
 
 echo "export GTK_IM_MODULE=fcitx" >> ~/.xprofile
 echo "export QT_IM_MODULE=fcitx" >> ~/.xprofile
